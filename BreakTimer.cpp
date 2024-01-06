11654bce9739973c223f526189b2571c3250ae25
brendan <brendanmichaud7@gmail.com>
2024-01-05 19:12:09 -0500

    Format and Comments

--- BreakTimer.cpp
+++ BreakTimer.cpp
@@ -3,45 +3,80 @@
 #include <QMessageBox>
 #include <QProcess>
 #include <QSystemTrayIcon>
+
+// Constructor
 BreakTimer::BreakTimer(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::BreakTimer), breakTime(0), timerId(-1) {
     ui->setupUi(this);
 
+    // Connect button signals to slots
     connect(ui->startButton, &QPushButton::clicked, this, &BreakTimer::startTimer);
     connect(ui->stopButton, &QPushButton::clicked, this, &BreakTimer::stopTimer);
+
+    // Connect spin box signal to the checkValue slot
+    connect(ui->breakTimeSpinBox, SIGNAL(valueChanged(int)), this, SLOT(checkValue(int)));
+
+    // Initial check of the spin box value
+    checkValue();
 }
 
+// Destructor
 BreakTimer::~BreakTimer() {
     delete ui;
 }
 
+// Slot to start the timer
 void BreakTimer::startTimer() {
-    breakTime = ui->breakTimeSpinBox->value() * 1000 * 60; // Convert minutes to milliseconds
+    breakTime = ui->breakTimeSpinBox->value() * minToMill; // Convert minutes to milliseconds
 
     if (timerId == -1) {
         // Start the timer only if it's not already running
         timerId = QObject::startTimer(breakTime);
+        ui->startButton->setEnabled(false);
+        ui->breakTimeSpinBox->setEnabled(false);
+        ui->stopButton->setEnabled(true);
     }
 }
 
+// Slot to stop the timer
 void BreakTimer::stopTimer() {
     if (timerId != -1) {
         // Stop the timer only if it's running
         killTimer(timerId);
         timerId = -1;
+        ui->startButton->setEnabled(true);
+        ui->breakTimeSpinBox->setEnabled(true);
+        ui->stopButton->setEnabled(false);
     }
 }
 
+// Slot to show a break message
 void BreakTimer::showBreakMessage() {
+    ui->startButton->setEnabled(true);
+    ui->breakTimeSpinBox->setEnabled(true);
+    ui->stopButton->setEnabled(false);
 
-
-        // Use GNotification for Wayland (GNOME)
-        QProcess::startDetached("notify-send", {"--expire-time=0", "Break Time", "It's time for a break!"});
+    // Use GNotification for Wayland (GNOME)
+    QProcess::startDetached("notify-send", {"--expire-time=0", "Break Time", "It's time for a break!"});
 }
 
+// Overridden function for handling timer events
 void BreakTimer::timerEvent(QTimerEvent *event) {
     if (event->timerId() == timerId) {
         stopTimer();  // Stop the timer after one cycle
         showBreakMessage();
+    }
+}
+
+// Slot to check the value of the spin box
+void BreakTimer::checkValue(int value) {
+    Q_UNUSED(value)
+
+    if (ui->breakTimeSpinBox->value() > 1) {
+        ui->startButton->setEnabled(true);
+        ui->stopButton->setEnabled(false);
+    } else {
+        ui->startButton->setEnabled(false);
+        ui->stopButton->setEnabled(false);
     }
 }
--- BreakTimer.h
+++ BreakTimer.h
@@ -1,32 +1,77 @@
+/**
+ * @file BreakTimer.h
+ * @brief Definition of the BreakTimer class.
+ */
+
 #ifndef BREAKTIMER_H
 #define BREAKTIMER_H
 
 #include <QMainWindow>
 #include <QTimer>
 
+const unsigned int minToMill = 1000 * 60;
+
+/**
+ * @brief Namespace containing the UI-related classes.
+ */
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class BreakTimer;
 }
 QT_END_NAMESPACE
 
+/**
+ * @class BreakTimer
+ * @brief A simple timer application for taking breaks.
+ */
 class BreakTimer : public QMainWindow {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor for the BreakTimer class.
+     * @param parent The parent widget.
+     */
     BreakTimer(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for the BreakTimer class.
+     */
     ~BreakTimer();
 
 private slots:
+    /**
+     * @brief Slot function to start the timer.
+     */
     void startTimer();
+
+    /**
+     * @brief Slot function to stop the timer.
+     */
     void stopTimer();
+
+    /**
+     * @brief Slot function to display a break message.
+     */
     void showBreakMessage();
+
+    /**
+     * @brief Overridden function for handling timer events.
+     * @param event The timer event.
+     */
     void timerEvent(QTimerEvent *event);
 
+    /**
+     * @brief Slot function to check the value of the spin box.
+     * @param value The current value of the spin box. Default is 0.
+     */
+    void checkValue(int value = 0);
+
 private:
-    Ui::BreakTimer *ui;
-    int breakTime;
-    int timerId;
+    Ui::BreakTimer *ui; ///< The UI object.
+
+    int breakTime; ///< Break time duration in milliseconds.
+    int timerId;   ///< Timer ID for managing the timer.
 };
 
 #endif // BREAKTIMER_H
